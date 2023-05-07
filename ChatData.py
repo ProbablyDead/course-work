ddf471cd1a492f5e94c210229cb017ba7c5a2d3a
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-05-07 16:17:19 +0300

    finished

---
+++ ChatData.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+import json
+
+class ChatData (Dataset):
+
+    def __init__(self, path:str, tokenizer) -> None:
+
+        self.data = json.load(open(path, 'r'))
+
+        self.units = []
+        for i in self.data:
+            self.units.append((i['question'], i['answer']))
+
+        for index, i in enumerate(self.units):
+            question, answer = i
+            self.units[index] = "<startofstring> " + question + " <bot>: " + answer + " <endofstring>"
+
+        self.units = self.units[:-1]
+
+        print(self.units[0])
+
+        self.units_encoded = tokenizer(self.units, truncation=True, padding=True, return_tensors="pt")
+        self.input_ids = self.units_encoded['input_ids']
+        self.attention_mask = self.units_encoded['attention_mask']
+
+    def __len__(self) -> int:
+        return len(self.units)
+
+    def __getitem__(self, index):
+        return (self.input_ids[index], self.attention_mask[index])
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from ChatData import ChatData
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import tqdm
+import torch
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def train (chatData, model, optim):
+    epochs = 10
+
+    for i in tqdm.tqdm(range(epochs)):
+        for unit, a in chatData:
+            optim.zero_grad()
+            loss = model(unit, attention_mask=a, labels=unit).loss
+            loss.backward()
+            optim.step()
+
+        torch.save(model.state_dict(), "model_state.pt")
+
+def infer(inp):
+    inp = "<startofstring> " + inp + " <bot>: "
+    inp = tokenizer(inp)
+
+    output = model.generate(**inp)
+    output = tokenizer.decode(output[0])
+    return output
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.add_special_tokens({"pad_token" : "<pad>",
+                              "bos_token" : "<startofstring>",
+                              "eos_token" : "<endofstring>"})
+tokenizer.add_tokens(["<bot>:"])
 
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+model.resize_token_embeddings(len(tokenizer))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+chatData = ChatData("sources/qa_Appliances.json", tokenizer)
+chatData = DataLoader(chatData, batch_size=64)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+model.train()
+
+optim = Adam(model.parameters())
+
