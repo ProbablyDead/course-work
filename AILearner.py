4806e9011ee323ff8d785cb66c3cdd4da7817bf7
ProbablyDead <volodin.is.03@mail.ru>
2023-05-09 15:41:30 +0300

    refactor

--- AILearner.py
+++ AILearner.py
@@ -5,40 +5,60 @@ from torch.utils.data import DataLoader
 import tqdm
 import torch
 
-def train (chatData, model, optim):
-    epochs = 10
+PT_FILE_PATH = "model_state.pt"
+PRETRAINED_MODEL = "gpt2"
+DATASET_PATH = "sources/datasets/humor_funny.json"
+DEFAULT_MESSAGE = "tell me an joke"
+EPOCHS = 10
+UNITS_TO_LEARN = 10000
+MAX_REQUEST_LENGTH = 40
+
+
+def train(chatData, model, optim):
+    epochs = EPOCHS
 
     for i in tqdm.tqdm(range(epochs)):
         for unit, a in chatData:
+            unit = unit.to(device)
+            a = a.to(device)
             optim.zero_grad()
             loss = model(unit, attention_mask=a, labels=unit).loss
             loss.backward()
             optim.step()
 
-        torch.save(model.state_dict(), "model_state.pt")
+        print(infer(DEFAULT_MESSAGE))
+
+        torch.save(model.state_dict(), PT_FILE_PATH)
+
 
 def infer(inp):
     inp = "<startofstring> " + inp + " <bot>: "
-    inp = tokenizer(inp)
+    inp = tokenizer(inp, return_tensors="pt")
 
-    output = model.generate(**inp)
+    unit = inp["input_ids"].to(device)
+    a = inp["attention_mask"].to(device)
+
+    output = model.generate(unit, attention_mask=a, max_length=MAX_REQUEST_LENGTH)
     output = tokenizer.decode(output[0])
+    output = output.split("<bot>:")[1]
+    output = output.replace("<pad>", '')
     return output
 
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.add_special_tokens({"pad_token" : "<pad>",
-                              "bos_token" : "<startofstring>",
-                              "eos_token" : "<endofstring>"})
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+tokenizer = GPT2Tokenizer.from_pretrained(PRETRAINED_MODEL)
+tokenizer.add_special_tokens({"pad_token": "<pad>",
+                              "bos_token": "<startofstring>",
+                              "eos_token": "<endofstring>"})
 tokenizer.add_tokens(["<bot>:"])
 
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained(PRETRAINED_MODEL)
 model.resize_token_embeddings(len(tokenizer))
 
-chatData = Chat("./datasets_json/entertainment_harrypotter.json", tokenizer)
+chatData = Chat(DATASET_PATH, tokenizer)
 chatData = DataLoader(chatData, batch_size=64)
 
 model.train()
 
-optim = Adam(model.parameters())
-
+optim = Adam(model.parameters(), lr=1e-3)
--- Chat.py
+++ Chat.py
@@ -1,9 +1,12 @@
 from torch.utils.data import Dataset
 import json
+NUMBER_OF_UNITS_TO_LEARN = 1000  # -1 is for whole dataset
+MAX_BOT_ANSWER_LENGTH = 80
+
 
 class Chat(Dataset):
 
-    def __init__(self, path:str, tokenizer) -> None:
+    def __init__(self, path: str, tokenizer) -> None:
 
         self.data = json.load(open(path, 'r'))
 
@@ -13,15 +16,16 @@ class Chat(Dataset):
 
         for index, i in enumerate(self.units):
             try:
-                self.units[index] = "<startofstring> " + i + " <bot>: " + self.units[index+1] + " <endofstring>"
+                self.units[index] = "<startofstring> " + i + " <bot>: " + self.units[index + 1] + " <endofstring>"
             except:
                 pass
 
-        self.units = self.units[:1000]
+        self.units = self.units[:NUMBER_OF_UNITS_TO_LEARN]
 
         print(self.units[0])
 
-        self.units_encoded = tokenizer(self.units, truncation=True, padding=True, return_tensors="pt")
+        self.units_encoded = tokenizer(self.units, max_length=MAX_BOT_ANSWER_LENGTH, truncation=True,
+                                       padding="max_length", return_tensors="pt")
         self.input_ids = self.units_encoded['input_ids']
         self.attention_mask = self.units_encoded['attention_mask']
 
@@ -29,4 +33,5 @@ class Chat(Dataset):
         return len(self.units)
 
     def __getitem__(self, index):
-        return (self.input_ids[index], self.attention_mask[index])
+
+        return self.input_ids[index], self.attention_mask[index]
